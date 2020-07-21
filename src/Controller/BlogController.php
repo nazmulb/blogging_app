396dd909f5be2f762d0c88fbcabb2b5602227bd3
nazmulbasher <nazmul.basher@fieldnation.com>
2020-07-22 00:10:22 +0300

    added codes

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Entity\Comment;
+use App\Form\CommentType;
 use App\Repository\PostRepository;
+use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -28,5 +31,15 @@ class BlogController extends AbstractController
     public function show(Post $post): Response
     {
         return $this->render('blog/post_show.html.twig', ['post' => $post]);
+    }
+
+    public function commentForm(Post $post): Response
+    {
+        $form = $this->createForm(CommentType::class);
+
+        return $this->render('blog/_comment_form.html.twig', [
+            'post' => $post,
+            'form' => $form->createView(),
+        ]);
     }
 }
