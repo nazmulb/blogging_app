cc7045ec0e486682338637d5b31815b55d4510fd
nazmulbasher <nazmul.basher@fieldnation.com>
2020-07-22 23:10:27 +0300

    added codes

--- src/Controller/Admin/BlogController.php
+++ src/Controller/Admin/BlogController.php
@@ -9,9 +9,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/admin/post")
+ * @IsGranted("ROLE_ADMIN")
  */
 class BlogController extends AbstractController
 {
