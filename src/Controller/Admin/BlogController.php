085a6bc43c7100123ef664749fc9369ec57b9478
nazmulbasher <nazmul.basher@fieldnation.com>
2020-07-22 14:11:37 +0300

    added codes

---
+++ src/Controller/Admin/BlogController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/post")
+ */
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/", methods="GET", name="admin_index")
+     * @Route("/", methods="GET", name="admin_post_index")
+     */
+    public function index(PostRepository $postRepository): Response
+    {
+        return $this->render('admin/blog/index.html.twig', [
+            'posts' => $postRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="admin_post_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($post);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_post_index');
+        }
+
+        return $this->render('admin/blog/new.html.twig', [
+            'post' => $post,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="admin_post_show", methods={"GET"})
+     */
+    public function show(Post $post): Response
+    {
+        return $this->render('admin/blog/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="admin_post_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Post $post): Response
+    {
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin_post_index');
+        }
+
+        return $this->render('admin/blog/edit.html.twig', [
+            'post' => $post,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="admin_post_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Post $post): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$post->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($post);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_post_index');
+    }
+}
