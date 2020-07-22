685d1ae16f6ba43d691c70d5a9a61d125bd81af3
nazmulbasher <nazmul.basher@fieldnation.com>
2020-07-22 11:08:29 +0300

    added codes

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,19 +6,39 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../scss/app.scss';
+import "../scss/app.scss";
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-import $ from 'jquery';
+import $ from "jquery";
 
 // JS for public website
 
-var greet = function(name) {
-    return `Yo yo ${name} - welcome to Encore!`;
+const resetMsg = function () {
+    $('#msgCmt').html('');
 };
 
-$(document).ready(function() {
-    //$('.footer-text').prepend(greet('jill'));
+$(document).ready(function () {
+  //$('.footer-text').prepend(greet('jill'));
+
+  $("#postCommentForm").submit(function (e) {
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+
+    var form = $(this);
+
+    $.ajax({
+      type: form.attr("method"),
+      url: form.attr("action"),
+      data: form.serialize(), // serializes the form's elements.
+      success: function (data) {
+        form.trigger("reset");
+        $('#newPostComments').append(`<p>${data.message}</p>`);
+      },
+      error: function (data) {
+        console.log(data);
+        $('#msgCmt').append('<p class="alert alert-danger">Something went wrong.</p>');
+
+        setTimeout(resetMsg, 3000);
+      },
+    });
+  });
 });
-
-
--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 
 class BlogController extends AbstractController
@@ -49,11 +50,10 @@ class BlogController extends AbstractController
             $entityManager->persist($comment);
             $entityManager->flush();
 
-            //return $this->json(['message' => 'Added successfully', 'type' => 'success']);
-            return $this->redirectToRoute('blog_post', ['slug' => $post->getSlug()]);
+            return $this->json(['message' => 'Added successfully.', 'type' => 'success']);
         }
 
-        return $this->json(['message' => 'Comment should not be empty', 'type' => 'error']);
+        throw new HttpException(400, 'Bad request');
     }
 
     public function commentForm(Post $post): Response
