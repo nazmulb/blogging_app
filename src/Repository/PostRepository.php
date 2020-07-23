4d11499a3ea9e9b8c03a54512fcb8718af7b4392
nazmulbasher <nazmul.basher@fieldnation.com>
2020-07-23 11:23:43 +0300

    added codes

--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -40,16 +40,4 @@ class PostRepository extends ServiceEntityRepository
             ->getResult()
         ;
     }
-
-    /*
-    public function findOneBySomeField($value): ?Post
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
