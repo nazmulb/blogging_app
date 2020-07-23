44d721d0656035711cc127b01f9eee801c321ab8
nazmulbasher <nazmul.basher@fieldnation.com>
2020-07-23 11:45:49 +0300

    added codes

--- src/Controller/Admin/BlogController.php
+++ src/Controller/Admin/BlogController.php
@@ -12,6 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
+ * Require ROLE_ADMIN for *every* controller method in this class.
  * @Route("/admin/post")
  * @IsGranted("ROLE_ADMIN")
  */
